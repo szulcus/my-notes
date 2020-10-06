1ca4a94978d5a4a0c25030d339623d004ea139d0
jakub104 <jakubzlezena@wp.pl>
2020-10-06 20:20:57 +0200

    deploy

---
+++ precache-manifest.6ca454c5f1b2f6d58bf78268171cfc74.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8105c2909ad06e42090f",
+    "url": "/na-j/css/app.f7910b15.css"
+  },
+  {
+    "revision": "33a32b21d5cdde378fa7",
+    "url": "/na-j/css/chunk-51db9ccc.e8921e5d.css"
+  },
+  {
+    "revision": "3af542978490f99fa494",
+    "url": "/na-j/css/chunk-5cbd8da6.d59a6002.css"
+  },
+  {
+    "revision": "2898e8817300ade380e3",
+    "url": "/na-j/css/chunk-ad39f570.4d274aa1.css"
+  },
+  {
+    "revision": "c0e5400332f179a7f837f1fd4f1ac33e",
+    "url": "/na-j/img/contact.c0e54003.jpg"
+  },
+  {
+    "revision": "edd535f52fc476103422488ea2efbfb3",
+    "url": "/na-j/img/home.edd535f5.jpeg"
+  },
+  {
+    "revision": "dc9bad0aea385a66a74b3684583dcf0d",
+    "url": "/na-j/img/logo.dc9bad0a.svg"
+  },
+  {
+    "revision": "9b1e70e13858b779ce337adee0e12121",
+    "url": "/na-j/img/offer.9b1e70e1.jpg"
+  },
+  {
+    "revision": "32f86e66e2b5b483afad08562084e9c8",
+    "url": "/na-j/img/realizations.32f86e66.jpg"
+  },
+  {
+    "revision": "85d42fd62ba3ca450132704b89e390e4",
+    "url": "/na-j/index.html"
+  },
+  {
+    "revision": "8105c2909ad06e42090f",
+    "url": "/na-j/js/app.71429e87.js"
+  },
+  {
+    "revision": "33a32b21d5cdde378fa7",
+    "url": "/na-j/js/chunk-51db9ccc.1057feb0.js"
+  },
+  {
+    "revision": "3af542978490f99fa494",
+    "url": "/na-j/js/chunk-5cbd8da6.e9c175c3.js"
+  },
+  {
+    "revision": "2898e8817300ade380e3",
+    "url": "/na-j/js/chunk-ad39f570.fe9cb248.js"
+  },
+  {
+    "revision": "41ed4bf14385ad70f5ea",
+    "url": "/na-j/js/chunk-vendors.942d4172.js"
+  },
+  {
+    "revision": "3783f8266b2976345441a5e805c5bf66",
+    "url": "/na-j/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/na-j/robots.txt"
+  }
+]);
